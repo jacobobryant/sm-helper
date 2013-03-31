69d5245daa6760721f5f438c2026b9bddfc0ac2c
Jacob O'Bryant <tooke@gmx.com>
2013-03-31 00:09:56 -0700

    added crash reporting with ACRA

---
+++ src/com/jacobobryant/scripturemastery/SMHelperApp.java
@@ -0,0 +1,26 @@
+package com.jacobobryant.scripturemastery;
+
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.ReportingInteractionMode;
+
+import android.app.Application;
+
+@ReportsCrashes(
+    formKey="",
+    mailTo = "tooke@gmx.com",
+    mode = ReportingInteractionMode.DIALOG,
+    resToastText = R.string.crash_toast_text,
+    resDialogText = R.string.crash_dialog_text,
+    resDialogTitle = R.string.crash_dialog_title,
+    resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
+    resDialogOkToast = R.string.crash_dialog_ok_toast
+    )
+public class SMHelperApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+    }
+}
